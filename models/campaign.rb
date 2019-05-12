3d38e603affa79cd2f9d1986600649e1c2c6a521
Ali Ibrahim <aliibrahim@gmail.com>
2019-05-12 22:57:34 +0500

    Added a method to find difference between remote and local campaign

--- models/campaign.rb
+++ models/campaign.rb
@@ -4,4 +4,30 @@ class Campaign < ActiveRecord::Base
   validates :status, inclusion: { in: STATUSES }
   validates :external_reference, presence: true
 
+  ATTRS_MAPPER = {
+    status: 'status',
+    ad_description: 'description',
+    external_reference: 'reference'
+  }
+
+  def diff(remote_campaign)
+    diff_hash = {
+      remote_reference: remote_campaign['reference'],
+      discrepancies: []
+    }
+    ATTRS_MAPPER.keys.each do |attr|
+      remote_value = remote_campaign[ATTRS_MAPPER[attr]]
+      local_value  = self.send(attr.to_sym)
+      if remote_value && remote_value != local_value
+        diff_hash[:discrepancies] << {
+          "#{ATTRS_MAPPER[attr]}": {
+            'local': local_value,
+            'remote': remote_value
+          }
+        }
+      end
+    end
+    diff_hash[:discrepancies].empty? ? {} : diff_hash
+  end
+
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,10 +1,63 @@
 require 'spec_helper'
-
+require 'json'
 RSpec.describe Campaign, type: :model do
+
+  let(:campaign) { Campaign.create(external_reference: '1', status: 'active', ad_description: 'Some Description') }
 
   context 'activerecord validations' do
     it { should validate_inclusion_of(:status).in_array(%w[active paused deleted]) }
     it { should validate_presence_of(:external_reference) }
+  end
+
+  context '#diff' do
+    context 'when there are differences between local and remote campaigns' do
+      let(:remote_campaign) {
+        {
+          reference: '1',
+          status: 'paused',
+          description: 'Description for campaign 11'
+        }.to_json
+      }
+      let(:difference) {
+        {
+          remote_reference: '1',
+          discrepancies: [
+            {
+              status: {
+                local:"active",
+                remote:"paused"
+              }
+            },
+            {
+              description:
+              {
+                local: "Some Description",
+                remote: "Description for campaign 11"
+              }
+            }
+          ]
+        }
+      }
+      it 'return the difference between local and remote version of campaign' do
+        expect(campaign.diff(JSON.parse(remote_campaign))).to eq(difference)
+      end
+    end
+
+    context 'when there are NO differences between local and remote campaigns' do
+
+      let(:remote_campaign) {
+        {
+          reference: '1',
+          status: 'active',
+          description: 'Some Description'
+        }.to_json
+      }
+      let(:difference) { {} }
+
+      it 'return the difference between local and remote version of campaign' do
+        expect(campaign.diff(JSON.parse(remote_campaign))).to eq(difference)
+      end
+    end
   end
 
 end
